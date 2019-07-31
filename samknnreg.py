7e69b76015213d083e41662f08b337d9876f4eff
Augustin Harter & Yannik Sander <{aharter,ysander}@techfak.uni-bielefeld.de>
2019-07-31 16:09:00 +0200

    Implement first set of functions

--- samknnreg.py
+++ samknnreg.py
@@ -1,11 +1,15 @@
 import numpy as np
+import math
 import sklearn.neighbors as sk
 from skmultiflow.core import RegressorMixin
-from skmultiflow.utils.utils import *
+#from skmultiflow.utils.utils import *
 
 class SAMKNNREG(RegressorMixin):
 
     def __init__(self, n_neighbors=5, max_LTM_size=5000, leaf_size=30, nominal_attributes=None):
+        """
+        test text
+        """
         self.n_neighbors = n_neighbors
         self.max_LTM_size = max_LTM_size
         self.STMX, self.STMy, self.LTMX, self.LTMy = ([], [], [], [])
@@ -37,13 +41,13 @@ class SAMKNNREG(RegressorMixin):
         self
 
         """
-        r, f = get_dimensions(X)
+        r = X.shape[0]
         for i in range(r):
+            print("fitting:", X[i,:], y[i])
             self._partial_fit(X[i,:], y[i])
 
     def _partial_fit(self, x, y):
         self._evaluateMemories(x, y)
-
         self._cleanLTM(x, y)
 
         self.STMX.append(x)
@@ -52,23 +56,55 @@ class SAMKNNREG(RegressorMixin):
         self._adaptSTM()
 
     def _cleanLTM(self, x, y):
-        tree = sk.KDTree(self.STMX, self.leaf_size, metric='euclidean')
-        dist, ind = tree.query(np.asarray(x), k=self.n_neighbors)
+    
+        tree = sk.KDTree(np.array(self.STMX), self.leaf_size, metric='euclidean')
+        dist, ind = tree.query(np.asarray([x]), k=self.n_neighbors)
+        dist = dist[0]
+        dist = np.where(dist < 0.1, 0.1, dist)
+        ind = ind[0]
         dmax = np.amax(dist)
-        dist = np.where(dist<1, dist, np.ones_like(dist))
-        qmax = np.amax(np.abs( (self.STMy[ind] - y) / (dist**2) )) 
+        qmax = np.amax(np.abs( (np.array(self.STMy)[ind] - y) / (dist**2) ))
 
-        tree = sk.KDTree(self.LTMX, self.leaf_size, metric='euclidean')
-        ind, dist = tree.query_radius(np.asarray(x), dmax, return_distance=True)
+        tree = sk.KDTree(np.array(self.LTMX), self.leaf_size, metric='euclidean')
+        ind, dist = tree.query_radius(np.asarray([x]), dmax, return_distance=True)
+        dist = dist[0]
+        dist = np.where(dist < 0.01, 0.01, dist)
+        ind = ind[0]
+        qtest = np.abs( (np.array(self.LTMy)[ind] - y) / (dist**2) )
+        dirty = ind[np.nonzero(qtest > qmax)]
+        for entry in dirty:
+            self.LTMX.__delitem__(entry)
+        #self.LTMX = list(np.delete(np.array(self.LTMX), dirty, axis=0))
 
+    def _predict(self, X, y, x):
+        X = np.asarray(X)
+        y = np.asarray(y)
 
+        tree = sk.KDTree(X, self.leaf_size, metric='euclidean')
+        dist, ind = tree.query(np.asarray(np.asarray([x])), k=self.n_neighbors)
+        dist = dist[0]
+        dist = np.where(dist < 0.01, 0.01, dist)
+        ind = ind[0]
+        norm = np.sum(1/dist)
+        inverse_weighted = (y[ind]) / dist
+        pred = ( np.sum(inverse_weighted) / norm )
+        return pred
 
+    def STMpredict(self, x):
+        return self._predict(self.STMX, self.STMy, x)
 
+    def LTMpredict(self, x):
+        return self._predict(self.LTMX, self.LTMy, x)
+
+    def COMBpredict(self, x):
+        return self._predict(self.STMX + self.LTMX, self.STMy + self.LTMy, x)
 
     def _evaluateMemories(self, x, y):
-        self.STMerror += (self.STMpredict(x)-y)**2
-        self.LTMerror += (self.LTMpredict(x)-y)**2
-        self.COMBerror += (self.COMBpredict(x)-y)**2
+
+        self.STMerror += math.log(abs(self.STMpredict(x)-y))
+        self.LTMerror += math.log(abs(self.LTMpredict(x)-y))
+        self.COMBerror += math.log(abs(self.COMBpredict(x)-y))
+        print("Errors:  STM: ", self.STMerror, "  LTM: ", self.LTMerror, "  COMB: ", self.COMBerror)
 
     def _adaptSTM(self):
         pass
@@ -76,7 +112,7 @@ class SAMKNNREG(RegressorMixin):
     def predict(self, X):
         """ predict
         
-        Predicts the label of the X sample, by searching the KDTree for 
+        Predicts the value of the X sample, by searching the KDTree for 
         the n_neighbors-Nearest Neighbors.
         
         Parameters
@@ -87,81 +123,48 @@ class SAMKNNREG(RegressorMixin):
         Returns
         -------
         list
-            A list containing the predicted labels for all instances in X.
+            A list containing the predicted values for all instances in X.
         
         """
-        r, c = get_dimensions(X)
-        proba = self.predict_proba(X)
-        predictions = []
-        for i in range(r):
-            predictions.append(np.argmax(proba[i]))
-
-        tree = sk.KDTree(self.window.get_attributes_matrix(), self.leaf_size, metric='euclidean')
-        dist, ind = tree.query(np.asarray(X), k=self.n_neighbors)
-
-        return np.array(predictions)
-
-    def predict_proba(self, X):
-        """ predict_proba
-         
-        Calculates the probability of each sample in X belonging to each 
-        of the labels, based on the knn algorithm.
         
-        Parameters
-        ----------
-        X: Numpy.ndarray of shape (n_samples, n_features)
-        
-        Raises
-        ------
-        ValueError: If there is an attempt to call this function before, 
-        at least, n_neighbors samples have been analyzed by the learner, a ValueError
-        is raised.
-        
-        Returns
-        -------
-        numpy.ndarray
-            An array of shape (n_samples, n_features), in which each outer entry is 
-            associated with the X entry of the same index. And where the list in 
-            index [i] contains len(self.target_value) elements, each of which represents
-            the probability that the i-th sample of X belongs to a certain label.
-         
-        """
-        #if self.window is None or self.window.n_samples < self.n_neighbors:
-        #    raise ValueError("KNN must be (partially) fitted on n_neighbors samples before doing any prediction.")
-        proba = []
-        r, c = get_dimensions(X)
+        mem_list = [self.STMerror, self.LTMerror, self.COMBerror]
+        best_mem_ind = np.argmin(mem_list)
+        if (best_mem_ind == 0): 
+            return np.array([self.STMpredict(x) for x in X])
+        elif (best_mem_ind == 1): 
+            return np.array([self.LTMpredict(x) for x in X])
+        elif (best_mem_ind == 2): 
+            return np.array([self.COMBpredict(x) for x in X])
 
-        self.classes = list(set().union(self.classes, np.unique(self.window.get_targets_matrix())))
+    def fit(self, X, y):
+        if (len(self.LTMX) < 10):
+            self.LTMX = list(X[0:10,:]) 
+            self.STMX = list(X[0:10,:]) 
+            self.LTMy = list(y[0:10])
+            self.STMy = list(y[0:10])
+            self.partial_fit(X[10:,:], y[10:])
+        else:
+            self.partial_fit(X, y)
 
-        new_dist, new_ind = self.__predict_proba(X)
-        for i in range(r):
-            votes = [0.0 for _ in range(int(max(self.classes) + 1))]
-            for index in new_ind[i]:
-                votes[int(self.window.get_targets_matrix()[index])] += 1. / len(new_ind[i])
-            proba.append(votes)
+    def predict_proba(self):
+        pass
 
-        return np.array(proba)
+    def print_model(self):
+        print("Errors:  STM: ", self.STMerror, "  LTM: ", self.LTMerror, "  COMB: ", self.COMBerror)
+        print("LTM:")
+        print(self.LTMX)
+        print("STM:")
+        print(self.STMX)
 
-    def __predict_proba(self, X):
-        """ __predict_proba
-        
-        Private implementation of the predict_proba method.
-        
-        Parameters
-        ----------
-        X: Numpy.ndarray of shape (n_samples, n_features)
-        
-        Returns
-        -------
-        tuple list
-            One list with the k-nearest neighbor's distances and another 
-            one with their indexes.
-        
-        """
-        # To use our own KDTree implementation please replace it as follows
-        # tree = KDTree(self.window.get_attributes_matrix(), metric='euclidean',
-        #              nominal_attributes=self._nominal_attributes, return_distance=True)
-
-        tree = sk.KDTree(self.window.get_attributes_matrix(), self.leaf_size, metric='euclidean')
-        dist, ind = tree.query(np.asarray(X), k=self.n_neighbors)
-        return dist, ind
+if __name__ == "__main__":
+    
+    X = np.arange(0, 1000, 5)
+    np.random.shuffle(X)
+    y = X**2
+    X = np.reshape(X, (X.shape[0], -1))
+    print(X)
+    print(y)
+    model = SAMKNNREG()
+    model.fit(X, y)
+    print(model.predict(np.array([[3],[8],[15],[79]])))
+    #model.print_model()
